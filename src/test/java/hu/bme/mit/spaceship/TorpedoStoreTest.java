6fe1bba7b15d25ce1af91712df4bdb8c7d6db553
Bence Tóth <me@bnctth.dev>
2023-10-25 17:37:24 +0200

    add test cases

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,10 +1,12 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 import org.junit.jupiter.api.Test;
 
 class TorpedoStoreTest {
+
 
   @Test
   void fire_Success(){
@@ -17,4 +19,20 @@ class TorpedoStoreTest {
     // Assert
     assertEquals(true, result);
   }
+
+  @Test
+  void fire_uses_torpedo(){
+    TorpedoStore store = new TorpedoStore(1);
+
+    store.fire(1);
+    assertEquals(0,store.getTorpedoCount());
+  }
+
+  @Test
+  void negative_torpedo(){
+    TorpedoStore store = new TorpedoStore(1);
+
+    assertThrowsExactly(IllegalArgumentException.class, ()->store.fire(-1));
+  }
+    
 }
